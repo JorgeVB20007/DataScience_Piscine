dd4c94cb2f82c8dcff71d432b567bf52e6a67903
jvacaris <84627746+JorgeVB20007@users.noreply.github.com>
2024-04-15 21:19:01 +0200

    ex01 done

--- ds2/ex01/chart.py
+++ ds2/ex01/chart.py
@@ -53,10 +53,16 @@ def main():
 	tot_income = []
 	avg_spending = []
 	for date, group_df in grouped:
+		# print(group_df)
 		dates.append(date)
 		tot_users.append(group_df.count()[0])
-		# tot_income.append(group_df[2].sum())
-		# print(">>>", date, "<<<", group_df.count()[0], group_df[2].sum())
+
+		grouped_users = group_df.groupby(group_df[1])
+		daily_user_spending = []
+		for user, group_usr in grouped_users:
+			daily_user_spending.append(group_usr[2].sum())
+		avg_spending.append(sum(daily_user_spending) / len(daily_user_spending))
+
 
 	dates_month = []
 	grouped = df.groupby(pd.Grouper(key=0, freq='ME'))
@@ -64,68 +70,29 @@ def main():
 		month = month.replace(day=1)
 		dates_month.append(month)
 		tot_income.append(group_df[2].sum())
-		# print(">>>", month, "<<<", group_df[2])
 	dates_month_name = [x.strftime("%b") for x in dates_month]
 
-	# print(df)
+	plt.figure(200)
+	plt.plot(dates, tot_users)
+	plt.xticks(ticks=dates_month, labels=dates_month_name)
+
+	plt.xlabel("month")
+	plt.ylabel("Number of customers")
 
 
-	# while (current_time < end_time):
-	# 	cur.execute('SELECT COUNT(*) FROM purchasers WHERE "event_time" >= \'{0}\' AND "event_time" < \'{1}\''.format(current_time, current_time + datetime.timedelta(days=1)))
-	# 	customer_nbr.append(cur.fetchone()[0])
-	# 	dates.append(current_time)
-	# 	cur.execute('SELECT * FROM purchasers WHERE "event_time" >= \'{0}\' AND "event_time" < \'{1}\''.format(current_time, current_time + datetime.timedelta(days=1)))
-	# 	full_values[current_time] = cur.fetchall()
-	# 	current_time += datetime.timedelta(days=1)
+	plt.figure(300)
+	plt.bar(dates_month, tot_income, width=25)
+	plt.xticks(ticks=dates_month, labels=dates_month_name)
 
-	fig = plt.figure()
+	plt.ylabel("month")
+	plt.ylabel("total sales in million of ₳")
 
-	ax = fig.add_subplot(111)
-	ax1 = fig.add_subplot(211)
-	ax2 = fig.add_subplot(212)
-#	ax3 = fig.add_subplot(222)
+	plt.figure(400)
+	plt.stackplot(dates, avg_spending)
+	plt.xticks(ticks=dates_month, labels=dates_month_name)
 
-
-	ax1.plot(dates, tot_users)
-	ax2.bar(dates_month, tot_income, width=25)
-
-	ax1.set_xticklabels(dates_month_name)
-	ax1.set_xlabel("month")
-	ax1.set_ylabel("Number of customers")
-
-	ax2.set_xticks(dates_month_name)
-	ax2.set_xticklabels(dates_month_name)
-	ax2.set_ylabel("month")
-	ax2.set_ylabel("total sales in million of ₳")
-
-
-
-	# fig, ax = plt.subplots(2, 2, sharex=True, sharey=False, )
-
-	# plt.figure(1)
-	# ax[0, 0].plot(dates, tot_users)
-
-	# print(dates_month)
-	# print(tot_income)
-
-
-
-
-
-
-	# ax[0, 1].bar(dates_month, tot_income, width=10)
-	# plt.gca().set_xticks(dates_month)
-	# plt.gca().set_xticklabels(['Oct', 'Nov', 'Dec', 'Jan', 'Feb'])
-	# plt.xlabel("month")
-	# # ax[0, 1].ylabel("total sales in million of ₳")
-
-
-
-	# # ax[0, 0].plot(dates, tot_users, labels=['Oct', 'Nov', 'Dec', 'Jan', 'Feb'])
-	# # ax[1, 0].bar(dates, tot_income, labels=['Oct', 'Nov', 'Dec', 'Jan', 'Feb'])
-
-
-
+	plt.ylabel("month")
+	plt.ylabel("average spend/customers in ₳")
 
 
 
@@ -133,32 +100,6 @@ def main():
 		plt.show()
 	except KeyboardInterrupt as msg:
 		print(msg)
-
-
-	# try:
-	# 	plt.show()
-	# except KeyboardInterrupt as msg:
-	# 	print(msg)
-
-	# view     = 9_654_310
-	# cart     = 5_486_521
-	# rfc      = 2_748_980
-	# purchase = 1_286_088
-
-
-	# labels = ['view', 'cart', 'remove_form_cart', 'purchase']
-	# values = [view, cart, rfc, purchase]
-
-
-	# fig, ax = plt.subplots()
-
-	# ax.pie(values, labels=labels, autopct='%1.1f%%', wedgeprops = {"edgecolor" : "white", 'linewidth': 0.5})
-	# print(rfc, cart, purchase, view)
-
-	# try:
-	# 	plt.show()
-	# except KeyboardInterrupt as msg:
-	# 	print(msg)
 
 	cur.close()
 	conn.close()
